5e0b10332dbbf32be1723cd01ce93b471bc619f0
Dinesh Pawar <dsp70458@gmail.com>
2024-10-31 18:53:47 +0530

    Add more Features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+// src/App.js
+
 import './App.css';
 import Header from './homepage/Header';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import FAQs from './pages/FAQs'
-import SampleTTS from './homepage/SampleTTS'
-import Middle from './homepage/Middle'
+import FAQs from './pages/FAQs';
+import SampleTTS from './homepage/SampleTTS';
+import Middle from './homepage/Middle';
+import Testimonial from './homepage/Testimonial';
+import FAQ from './homepage/FAQ';
+import Footer from './homepage/Footer'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Import your other components as needed
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
-        < SampleTTS />
-        < Middle />
+        <SampleTTS />
+        <Middle />
+        <Testimonial />
+        <FAQ />
+        <Footer />
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/about" element={<About />} /> 
+          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/faqs" element={<FAQs />} />
           <Route path="/pricing" element={<FAQs />} />
